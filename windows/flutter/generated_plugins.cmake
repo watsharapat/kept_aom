af8f7b98c8c6d3d21fa9945de87a7cec1ad3e675
watsharapat <watsharapat.s@outlook.com>
2026-07-23 16:28:10 +0700

    แก้ไขหน้า Login, ปรับรูปแบบ Key ไปใช้ Publishable

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,10 +5,13 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   emoji_picker_flutter
+  file_selector_windows
+  passkeys_windows
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
